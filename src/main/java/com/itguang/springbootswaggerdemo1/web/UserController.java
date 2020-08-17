9198c9a208464354aada4591acdadaaeb407b9fc
ithuange <1156421151@qq.com>
2020-08-17 17:27:06 +0800

    测试2号

---
+++ src/main/java/com/itguang/springbootswaggerdemo1/web/UserController.java
@@ -0,0 +1,20 @@
+package com.itguang.springbootswaggerdemo1.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author itguang
+ * @create 2018-01-05 8:23
+ **/
+@RestController
+public class UserController {
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String hello(){
+        System.out.println("这是测试二号");
+        return "hello";
+    }
+
+}
